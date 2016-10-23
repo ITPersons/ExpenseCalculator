98988797cb5b15ecff5d8182224457fbdef13b7e
deXter <mrdx71@gmail.com>
2016-10-24 00:43:17 +0500

    siri

--- app/src/main/java/com/example/zohaibsiddique/expensecalculator/DB.java
+++ app/src/main/java/com/example/zohaibsiddique/expensecalculator/DB.java
@@ -1,7 +1,7 @@
 package com.example.zohaibsiddique.expensecalculator;
 
 /**
- * Created by Zohaib Siddique on 17/06/2016.
+ * Created by deXter on 17/06/2016.
  */
 import android.content.ContentValues;
 import android.content.Context;
